9126c8483c4395bae8718770199ecb091ae4a8ec
Riccardo Foti <riccardofoti97@gmail.com>
2024-02-12 15:30:28 +0000

    define policy and permission v1

---
+++ terraform/data.tf
@@ -0,0 +1,30 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "policy" {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "ListObjectsInBucket",
+            "Effect": "Allow",
+            "Action": ["s3:ListBucket"],
+            "Resource": ["arn:aws:s3:::bucket-name"]
+        },
+        {
+            "Sid": "AllObjectActions",
+            "Effect": "Allow",
+            "Action": "s3:*Object",
+            "Resource": ["arn:aws:s3:::bucket-name/*"]
+        }
+    ]
+}
---
+++ terraform/policies.tf
@@ -0,0 +1,3 @@
+data "aws_policy_document" "assume_role" {
+    
+}
