ee7b1717a439bb1ddab04d430d070ff3f3f11f86
nicolopr <139747499+nicolopr@users.noreply.github.com>
2024-02-13 12:00:38 +0100

    basic version of save_csv completed

--- src/lambda/extraction_lambda.py
+++ src/lambda/extraction_lambda.py
@@ -1,24 +1,39 @@
 import pprint
 from pg8000.native import Connection, identifier, literal
+import pandas as pd
 from dotenv import load_dotenv
 import os
 from utils.utils import *
 
 
-def connect():
+def save_db_to_csv():
+    '''
+    Null -> Null
+    This function connects to a server specified in the .env variable via pg8000.native
+    Extract all rows from the currency SQL table
+    Inputs them in a pandas dataframe
+    Saves the dataframe to a .csv file
+    '''
     load_dotenv()
-    con = Connection(
+    conn = Connection(
         host=os.environ["Hostname"],
         user=os.environ["Username"],
         password=os.environ["Password"],
         database=os.environ["Database_name"],
         port=os.environ["Port"],
     )
-    sql_query = f"SELECT * FROM currency;"
-    sql_results = con.run(sql_query)
-    con.close()
-    return sql_results
-
+    rows=conn.run('SELECT * FROM currency;')
+    cols_name_nested=conn.run('''SELECT COLUMN_NAME 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+TABLE_NAME = 'currency'
+''')
+    cols_name=[el[0] for el in cols_name_nested]
+    conn.close()
+    df=pd.DataFrame(rows)
+    df.index=df[0].values
+    df.columns=cols_name
+    df.to_csv('./file.csv', sep=',', index=False, encoding='utf-8')
 
 if __name__ == "__main__":
-    print(connect())
+    save_db_to_csv()
