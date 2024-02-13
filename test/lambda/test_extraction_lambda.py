94fd88336bee4644e533a8060b5bae8f1dddf9a4
nicolopr <139747499+nicolopr@users.noreply.github.com>
2024-02-13 17:22:12 +0100

    done some testing

--- test/lambda/test_extraction_lambda.py
+++ test/lambda/test_extraction_lambda.py
@@ -42,8 +42,7 @@ def test_save_table_empty_table():
 
 def test_save_table_table_with_one_row():
     '''
-    Check that pandas is called if we get one row from the db query, and that the resulting
-    df is saved to the appropriate file
+    Check that pandas is called if we get one row from the db query
     '''
     with patch("src.lambda_functions.extraction_lambda.Connection") as conn_patched:
         conn_patched.run.return_value=[['mock','data','nah']]
@@ -51,4 +50,14 @@ def test_save_table_table_with_one_row():
         with patch("src.lambda_functions.extraction_lambda.pd.DataFrame") as mock_df:
                 save_table_to_csv(conn_patched, 'table')
                 mock_df.assert_called_once_with([['mock','data','nah']])
-                mock_df.assert_called_once_with([['mock','data','nah']])
+
+def test_save_table_table_with_three_rows():
+    '''
+    Check that pandas is called if we get three row from the db query
+    '''
+    with patch("src.lambda_functions.extraction_lambda.Connection") as conn_patched:
+        conn_patched.run.return_value=[['mock','data','nah'],['mock','data','nah'],['mock','data','nah']]
+        conn_patched.columns=[{'name':'id'},{'name':'data'},{'name':'yayornay'}]
+        with patch("src.lambda_functions.extraction_lambda.pd.DataFrame") as mock_df:
+                save_table_to_csv(conn_patched, 'table')
+                mock_df.assert_called_once_with([['mock','data','nah'],['mock','data','nah'],['mock','data','nah']])
