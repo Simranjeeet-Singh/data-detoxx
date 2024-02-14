acde71c6210d57f10b9551b56936823c779f7bb0
Riccardo Foti <riccardofoti97@gmail.com>
2024-02-14 16:01:23 +0000

    Add permission to write to cloudwatch

--- terraform/policies.tf
+++ terraform/policies.tf
@@ -36,3 +36,33 @@ data "aws_iam_policy_document" "s3_rw_policy" {
     ]
   }
 }
+
+# Create Cloudwatch log group
+resource "aws_cloudwatch_log_group" "cw_log_group" {
+  name = "/aws/lambda/${aws_lambda_function.s3_file_reader.function_name}"
+#   "/aws/lambda/${aws_lambda_function.demo_lambda.function_name}"
+}
+
+# Create Cloudwatch logging policy
+resource "aws_iam_policy" "function_logging_policy" {
+  name   = "function-logging-policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        Action : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect : "Allow",
+        Resource : "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
+# Attach Cloudwatch logging policy to Lambda role
+resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
+  role = aws_iam_role.lambda_one_role.id
+  policy_arn = aws_iam_policy.function_logging_policy.arn
+}
