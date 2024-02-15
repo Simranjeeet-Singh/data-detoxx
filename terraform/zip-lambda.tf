39578fd6fa4ec11f9e5b8d2f44ff8f1cc90285ef
Riccardo Foti <riccardofoti97@gmail.com>
2024-02-15 10:17:27 +0000

    Add layer with all dependencies for lambda_one

--- terraform/zip-lambda.tf
+++ terraform/zip-lambda.tf
@@ -1,16 +1,23 @@
 resource "null_resource" "install_dependencies" {
   provisioner "local-exec" {
-    command = "pip install -r ${path.module}/../src/requirements.txt -t ${path.module}/../src/"
+    command = "pip install -r ${path.module}/../src/requirements.txt -t ${path.module}/../src/dependencies/python"
   }
 }
 
-data "archive_file" "lambda_zip" {
+data "archive_file" "lambda_dependencies_zip" {
   depends_on = [null_resource.install_dependencies]
   excludes   = [
     "__pycache__",
     "venv",
   ]
   type             = "zip"
-  source_dir       = "${path.module}/../src"
+  source_dir       = "${path.module}/../src/dependencies"
+  output_path      = "${path.module}/../lambda_dependencies.zip"
+}
+
+data "archive_file" "lambda_one_zip" {
+  depends_on = [data.archive_file.lambda_dependencies_zip]
+  type             = "zip"
   output_path      = "${path.module}/../lambda_one.zip"
+  source_file       = "${path.module}/../src/lambda_one.py"
 }
