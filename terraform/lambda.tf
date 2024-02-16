a8d5a8e40a0b03ad7e783d7108f034a7a0d25ddd
John He <john.jh.he@gmail.com>
2024-02-16 15:29:33 +0000

    lambda deployment working but no pandas

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "s3_file_reader" {
   depends_on = [data.archive_file.lambda_one_zip,aws_lambda_layer_version.lambda_one_dependencies]
-  filename = "${path.module}/../lambda_one.zip"
+  filename = "${path.module}/../tmp/lambda_one.zip"
   function_name = "lambda_handler"
   role          = aws_iam_role.lambda_one_role.arn
   handler       = "lambda_one.lambda_handler"
--- terraform/zip-lambda.tf
+++ terraform/zip-lambda.tf
@@ -1,6 +1,9 @@
 resource "null_resource" "install_dependencies" {
   provisioner "local-exec" {
-    command = "pip install -r ${path.module}/../src/lambda_code/requirements.txt -t ${path.module}/../src/dependencies/python"
+    command = "pip install -r ${path.module}/../src/lambda_code/requirements.txt -t ${path.module}/../tmp/dependencies/python"
+  }
+  triggers = {
+  always_run = timestamp()
   }
 }
 
@@ -11,13 +14,13 @@ data "archive_file" "lambda_dependencies_zip" {
     "venv",
   ]
   type             = "zip"
-  source_dir      = "${path.module}/../src/dependencies"
-  output_path      = "${path.module}/../lambda_dependencies.zip"
+  source_dir      = "${path.module}/../tmp/dependencies"
+  output_path      = "${path.module}/../tmp/lambda_dependencies.zip"
 }
 
 data "archive_file" "lambda_one_zip" {
   depends_on = [data.archive_file.lambda_dependencies_zip]
   type             = "zip"
-  output_path      = "${path.module}/../lambda_one.zip"
+  output_path      = "${path.module}/../tmp/lambda_one.zip"
   source_dir       = "${path.module}/../src/lambda_code"
 }
