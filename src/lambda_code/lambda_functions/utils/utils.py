65a36d50f15f645fc604a6f97a00156402f40faf
John He <john.jh.he@gmail.com>
2024-02-20 12:14:35 +0000

    fixed import module error

--- src/lambda_code/lambda_functions/utils/utils.py
+++ src/lambda_code/lambda_functions/utils/utils.py
@@ -1,5 +1,6 @@
 import boto3
-from lambda_functions.utils.date_utils import convert_utc_to_sql_timestamp
+from date_utils import convert_utc_to_sql_timestamp
+import pandas as pd
 
 
 def return_latest_counter_and_timestamp_from_filenames(
@@ -33,7 +34,8 @@ def return_latest_counter_and_timestamp_from_filenames(
 
     largest_counter = max(counter_timestamp_dict.keys())
     sql_datetime = convert_utc_to_sql_timestamp(
-        counter_timestamp_dict[largest_counter].strip(".csv"))
+        counter_timestamp_dict[largest_counter].strip(".csv")
+    )
     return (largest_counter, sql_datetime)
 
 
@@ -46,11 +48,15 @@ def list_files_from_s3(bucket_name: str) -> list[str]:
     response = client.list_objects(Bucket=bucket_name)
     if "Contents" not in response:
         return []
-    return [item["Key"].split('/')[-1] for item in response["Contents"]]
+    return [item["Key"].split("/")[-1] for item in response["Contents"]]
+
+
+def get_table_from_s3(bucket_name: str, table_name: str) -> pd.DataFrame:
+    pass
 
 
 if __name__ == "__main__":
-    filenames = ["mytable_[#1]_2009-08-08T121800Z",
-                 "mytable_[#2]_2009-08-08T1218020Z"]
-    print(return_latest_counter_and_timestamp_from_filenames("mytable", filenames))
-    print(return_latest_counter_and_timestamp_from_filenames("mytable", []))
+    # filenames = ["mytable_[#1]_2009-08-08T121800Z", "mytable_[#2]_2009-08-08T1218020Z"]
+    # print(return_latest_counter_and_timestamp_from_filenames("mytable", filenames))
+    # print(return_latest_counter_and_timestamp_from_filenames("mytable", []))
+    print(list_files_from_s3("data-detox-ingestion-bucket"))
