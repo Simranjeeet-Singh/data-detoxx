79a260f07e5eccbe381b2db0e7de3e7eeaa87ba5
Ciaran Kefford <ciarankefford@gmail.com>
2024-02-21 11:21:43 +0000

    add docstrings for transform tables functions

--- src/transform_design_table.py
+++ src/transform_design_table.py
@@ -2,6 +2,17 @@ import pandas as pd
 
 
 def transform_design_table(design_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Simplify a design table DataFrame by selecting specific columns of interest.
+
+    This function filters the input DataFrame to include only the columns related to design ID, design name, file location, and file name.
+
+    Parameters:
+    - design_df (pd.DataFrame): A DataFrame containing design-related data, expected to have at least the columns 'design_id', 'design_name', 'file_location', and 'file_name'.
+
+    Returns:
+    - pd.DataFrame: A new DataFrame that includes only the 'design_id', 'design_name', 'file_location', and 'file_name' columns from the original DataFrame.
+    """
     transformed_df = design_df[[
         "design_id", "design_name", "file_location", "file_name"]]
 
