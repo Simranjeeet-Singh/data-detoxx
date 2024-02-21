7e5bc4d5c93d286d6e4bd00d23c3c3b570ea4b23
Nicolò Primi <139747499+nicolopr@users.noreply.github.com>
2024-02-21 11:03:37 +0100

    Lambda 2 tf (#21)
    * doesnt work
    
    * test commit
    
    * fixed everything. Remember to revert the bucket names
    
    * seems to be working well, except the event trigger for lambda 2
    
    * last commit of day. Eventbridge for lambda 2 needs to be tested
    
    * fixed path in makefile and tests

--- terraform/alerts.tf
+++ terraform/alerts.tf
@@ -84,3 +84,90 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   endpoint  = "detoxdata7@gmail.com" #change to real email address
 }
 
+#Lambda 2
+resource "aws_cloudwatch_log_metric_filter" "syntax_error_2" {
+  depends_on = [ aws_cloudwatch_log_group.cw_log_group_2 ]
+  name           = "SyntaxMetricLog"
+  pattern        = "SyntaxError"
+  log_group_name = "/aws/lambda/${aws_lambda_function.s3_processor.function_name}"
+  metric_transformation {
+    name      = "SyntaxErrorCount"
+    namespace = "LogMetric"
+    value     = "1"
+  }
+}
+resource "aws_cloudwatch_log_metric_filter" "Runtime-Error_2" {
+  depends_on = [ aws_cloudwatch_log_group.cw_log_group_2 ]
+  name           = "RuntimeErrorLog"
+  pattern        = "RuntimeError"
+  log_group_name = "/aws/lambda/${aws_lambda_function.s3_processor.function_name}"
+  metric_transformation {
+    name      = "RunTimeErrorCount"
+    namespace = "LogMetric"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "value_error_2" {
+  depends_on = [ aws_cloudwatch_log_group.cw_log_group_2 ]
+  name           = "ValueMetricLog"
+  pattern        = "ValueError"
+  log_group_name = "/aws/lambda/${aws_lambda_function.s3_processor.function_name}"
+  metric_transformation {
+    name      = "ValueErrorCount"
+    namespace = "LogMetric"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda-syntax-error-alarm_2" {
+  alarm_name                = "lambda-syntax-error-count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = "SyntaxErrorCount"
+  namespace                 = "LogMetric"
+  period                    = 300 #change to correct period
+  statistic                 = "Sum"
+  threshold                 = 1
+  alarm_description         = "This metric monitors logs for syntax errors from lambda1"
+  alarm_actions             = [aws_sns_topic.error_alerts.arn]
+  insufficient_data_actions = []
+}
+resource "aws_cloudwatch_metric_alarm" "lambda-Runtime-Error-alarm_2" {
+  alarm_name                = "lambda-Runtime-Error-count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = "RunTimeErrorCount"
+  namespace                 = "LogMetric"
+  period                    = 300 #change to correct period
+  statistic                 = "Sum"
+  threshold                 = 1
+  alarm_description         = "This metric monitors logs for runtime errors from lambda1"
+  alarm_actions             = [aws_sns_topic.error_alerts.arn]
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda-value-error-alarm_2" {
+  alarm_name                = "lambda-value-error-count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = "ValueErrorCount"
+  namespace                 = "LogMetric"
+  period                    = 300 #change to correct period
+  statistic                 = "Sum"
+  threshold                 = 1
+  alarm_description         = "This metric monitors logs for value errors from lambda1"
+  alarm_actions             = [aws_sns_topic.error_alerts.arn]
+  insufficient_data_actions = []
+}
+
+resource "aws_sns_topic" "error_alerts_2" {
+  name = "lambda-error-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription_2" {
+  topic_arn = aws_sns_topic.error_alerts.arn
+  protocol  = "email"
+  endpoint  = "detoxdata7@gmail.com" #change to real email address
+}
+
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "ingestion_bucket" {
   bucket = "${var.ingestion_storage}"
 }
+
+resource "aws_s3_bucket" "processed_bucket" {
+  bucket = "${var.processed_storage}"
+}
