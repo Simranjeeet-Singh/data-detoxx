4b64dbacc8524850cddaa6e9403e324b5cdc38a0
Ciaran Kefford <ciarankefford@gmail.com>
2024-02-21 10:45:17 +0000

    add function to convert currency code to currency name with tests

---
+++ test/lambda/test_currency_code_to_string.py
@@ -0,0 +1,9 @@
+from src.currency_code_to_currency_name import currency_code_to_currency_name as cccn
+
+
+def test_function_returns_correct_string_for_inputs():
+    assert cccn('EUR') == 'Euro'
+
+
+def test_function_returns_error_message_if_code_not_found():
+    assert cccn('JPY') == 'Unknown currency code'
