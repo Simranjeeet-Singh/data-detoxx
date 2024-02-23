57c62e7718899fb8d30268caeb2a930d7f88de3a
Nicolò Primi <139747499+nicolopr@users.noreply.github.com>
2024-02-23 12:01:53 +0100

    Lambda 2 function (#31)
    * first attempt at writing the function. There's a lot of fake code that needs to be fixed
    
    * finished fact_sales_transformer + utils function with testing
    
    * continued writing lambda_2 function
    
    * added more structure and pseudocode to lambda_2
    
    * commit before merge
    
    * commit before merge main
    
    * trying to fix import
    
    * last commit before merging in main
    
    * refactored list_tables_from_s3

--- src/lambda_two/lambda_two.py
+++ src/lambda_two/lambda_two.py
@@ -1,13 +1,63 @@
 import logging
 import boto3
-
+import pandas as pd
+from src.utils.file_reading_utils import list_files_from_s3
+#TBD: import all transformation functions
 
 def lambda_handler2(event, context):
     logger = logging.getLogger("MyLogger")
     logger.setLevel(logging.INFO)
-    logger.error("error")
-    s3 = boto3.client("s3")
-    s3.put_object(
-        Bucket="data-detox-processed-bucket", Key="s3_key.txt", Body="file_data"
-    )
-    raise RuntimeError
+    tablenames=list(set([element.split('__')[0] for element in list_files_from_s3('data-detox-processed-bucket')]))
+    dataframes={}
+    for tablename in tablenames:
+        if tablename=='department' or tablename=='counterparty':
+            df=''#read all csvs for that table
+        else:
+            df='' #read only last updates via john_function('data-detox-ingestion-bucket', tablename)
+            dataframes[tablename]=df
+    #dataframes is a dictionary containining all dataframes with the last updated/added data 
+    processed_dataframes=process_dataframes(dataframes)
+    for tablename in process_dataframes.keys():
+        #save each df to appropriate parquet file in tmp with good name
+        #path structure is 'tablename/tablename__[#version]__date_last_updated.parquet'
+        #find_path should generate it
+        path=find_path(tablename)
+        process_dataframes[tablename].to_parquet('/tmp/'+path)
+    #upload tmp to the processed bucket
+    #to be written
+
+def process_dataframes(dataframes: dict[pd.DataFrame]) -> dict[pd.DataFrame]:
+    """
+    Process a dictionary of pandas dataframes representing all tables in the original database.
+    
+    Args:
+    - dataframes (Dict[str, pd.DataFrame]): A dictionary where keys represent table names and values are the corresponding pandas dataframes.
+    
+    Returns:
+    - processed_df_dict (Dict[str, pd.DataFrame]): A dictionary containing processed pandas dataframes with keys being the process table names.
+    
+    The function processes each dataframe using our transformation functions and stores them in a dictionary.
+    The keys of the resulting dictionary correspond to the processed table names, and the values are the processed dataframes.
+    The processed table names are: 'fact_sales_order', 'dim_date', 'dim_counterparty', 'dim_staff', 'dim_currency',
+    'dim_design', and 'dim_location'.
+    """
+    processed_df_dict={}
+
+    #transform_ functions need to be replaced with our actual transformation functions
+    # processed_df_dict['fact_sales_order']=transform_fact_sales_order(dataframes['sales_order'])
+    # processed_df_dict['dim_date']=transform_dim_date(dataframes['sales_order'])
+    # processed_df_dict['dim_counterparty']=transform_fact_sales_order(dataframes['counterparty'],dataframes['address'])
+    # processed_df_dict['dim_staff']=transform_fact_sales_order(dataframes['staff'],dataframes['department'])
+    # processed_df_dict['dim_currency']=transform_fact_sales_order(dataframes['currency'])
+    # processed_df_dict['dim_design']=transform_fact_sales_order(dataframes['design'])
+    # processed_df_dict['dim_location']=transform_fact_sales_order(dataframes['location'])
+    #add more transformations here if we go beyond MVP
+    return processed_df_dict
+
+def find_path(tablename):
+    #to be written
+    pass
+
+if __name__=='__main__':
+    lambda_handler2('test','test')
+
