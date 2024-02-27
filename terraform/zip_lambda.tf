080b5720c9af59bbcad85ac705194db772c7e035
JHE23 <139002131+JHE23@users.noreply.github.com>
2024-02-27 16:00:46 +0000

    Deploy lambda3 (#52)
    * changed to use pandas instead of pyarrow to read parquet
    
    * draft deployment of lambda3 not working yet needs pyarrow layer

--- terraform/zip_lambda.tf
+++ terraform/zip_lambda.tf
@@ -32,6 +32,17 @@ resource "null_resource" "install_lambda_two_dependencies" {
 
 }
 
+resource "null_resource" "install_lambda_three_dependencies" {
+  depends_on = [null_resource.clear_tmp]
+  provisioner "local-exec" {
+    command = "pip install -r ${path.module}/../src/lambda_three/lambda_requirements.txt -t ${path.module}/../tmp/lambda_three_dependencies/python"
+  }
+  triggers = {
+  always_run = timestamp()
+  }
+
+}
+
 resource "null_resource" "move_utils_to_tmp" {
   depends_on = [null_resource.clear_tmp]
   provisioner "local-exec" {
@@ -73,6 +84,17 @@ data "archive_file" "lambda_two_dependencies_zip" {
   type             = "zip"
   source_dir      = "${path.module}/../tmp/lambda_two_dependencies"
   output_path      = "${path.module}/../tmp/lambda_two_dependencies.zip"
+}
+
+data "archive_file" "lambda_three_dependencies_zip" {
+  depends_on = [null_resource.clear_tmp, null_resource.install_lambda_three_dependencies]
+  excludes   = [
+    "/python/__pycache__"  // Doesn't work for some reason??
+  ]
+
+  type             = "zip"
+  source_dir      = "${path.module}/../tmp/lambda_three_dependencies"
+  output_path      = "${path.module}/../tmp/lambda_three_dependencies.zip"
 }
 
 data "archive_file" "lambda_one_zip" {
