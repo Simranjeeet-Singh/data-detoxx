ef6b706d86f27c42cf7b8c6095517ce56c176908
nicolopr <139747499+nicolopr@users.noreply.github.com>
2024-02-27 11:53:51 +0100

    wrote terraform infrastructure for lambda_3

--- terraform/alerts.tf
+++ terraform/alerts.tf
@@ -170,3 +170,90 @@ resource "aws_sns_topic_subscription" "email_subscription_2" {
   protocol  = "email"
   endpoint  = "detoxdata7@gmail.com"
 }
+
+#Lambda 3
+resource "aws_cloudwatch_log_metric_filter" "syntax_error_3" {
+  depends_on = [ aws_cloudwatch_log_group.cw_log_group_3 ]
+  name           = "SyntaxMetricLog_3"
+  pattern        = "SyntaxError"
+  log_group_name = "/aws/lambda/${aws_lambda_function.s3_uploader.function_name}"
+  metric_transformation {
+    name      = "SyntaxErrorCount_3"
+    namespace = "LogMetric_3"
+    value     = "1"
+  }
+}
+resource "aws_cloudwatch_log_metric_filter" "Runtime-Error_3" {
+  depends_on = [ aws_cloudwatch_log_group.cw_log_group_3 ]
+  name           = "RuntimeErrorLog_3"
+  pattern        = "RuntimeError"
+  log_group_name = "/aws/lambda/${aws_lambda_function.s3_uploader.function_name}"
+  metric_transformation {
+    name      = "RunTimeErrorCount_3"
+    namespace = "LogMetric_3"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "value_error_3" {
+  depends_on = [ aws_cloudwatch_log_group.cw_log_group_3 ]
+  name           = "ValueMetricLog_3"
+  pattern        = "ValueError"
+  log_group_name = "/aws/lambda/${aws_lambda_function.s3_uploader.function_name}"
+  metric_transformation {
+    name      = "ValueErrorCount_3"
+    namespace = "LogMetric_3"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda-syntax-error-alarm_3" {
+  alarm_name                = "lambda3-syntax-error-count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = "SyntaxErrorCount_3"
+  namespace                 = "LogMetric_3"
+  period                    = 300
+  statistic                 = "Sum"
+  threshold                 = 1
+  alarm_description         = "This metric monitors logs for syntax errors from lambda3"
+  alarm_actions             = [aws_sns_topic.error_alerts_3.arn]
+  insufficient_data_actions = []
+}
+resource "aws_cloudwatch_metric_alarm" "lambda-Runtime-Error-alarm_3" {
+  alarm_name                = "lambda3-Runtime-Error-count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = "RunTimeErrorCount_3"
+  namespace                 = "LogMetric_3"
+  period                    = 300 
+  statistic                 = "Sum"
+  threshold                 = 1
+  alarm_description         = "This metric monitors logs for runtime errors from lambda3"
+  alarm_actions             = [aws_sns_topic.error_alerts_3.arn]
+  insufficient_data_actions = []
+}
+
+resource "aws_cloudwatch_metric_alarm" "lambda-value-error-alarm_3" {
+  alarm_name                = "lambda3-value-error-count"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = 1
+  metric_name               = "ValueErrorCount_3"
+  namespace                 = "LogMetric_3"
+  period                    = 300 
+  statistic                 = "Sum"
+  threshold                 = 1
+  alarm_description         = "This metric monitors logs for value errors from lambda3"
+  alarm_actions             = [aws_sns_topic.error_alerts_3.arn]
+  insufficient_data_actions = []
+}
+
+resource "aws_sns_topic" "error_alerts_3" {
+  name = "lambda3-error-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email_subscription_3" {
+  topic_arn = aws_sns_topic.error_alerts_3.arn
+  protocol  = "email"
+  endpoint  = "detoxdata7@gmail.com"
+}
