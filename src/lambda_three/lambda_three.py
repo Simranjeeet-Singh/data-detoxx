086368a3fce0bb85912fa0db3a69fca17e64f1e8
Riccardo Foti <riccardofoti97@gmail.com>
2024-02-28 11:15:14 +0000

    update lambda3 (#54)

--- src/lambda_three/lambda_three.py
+++ src/lambda_three/lambda_three.py
@@ -1,17 +1,14 @@
 import boto3
 
-import pyarrow.parquet as pq
-from io import BytesIO
 from pg8000.native import Connection
 from pg8000 import DatabaseError
 from utils.extract_secrets import get_secret
-from utils.date_utils import convert_datetime_to_utc
-import datetime
 import pandas as pd
 import logging
 from utils.state_file import read_state_file_from_s3
 from utils.file_reading_utils import return_latest_counter_and_timestamp_from_filenames, get_parquet_dataframe_from_s3, list_files_from_s3_folder
 
+BUCKET_NAME = "data-detox-processed-bucket"
 
 def connect():
     """
@@ -43,10 +40,26 @@ connect()
 
 
 def lambda_handler3(event, context):
+    """
+    It initializes a logger, connects to the final PostgreSQL Warehouse,
+    extracts data from last updated parquet file in processed bucket,
+    and inserts extracted data into the Warehouse.
+
+    Parameters:
+    - event: The event data (not used in this function, but required by AWS Lambda).
+    - context: The runtime context of the lambda (not used in this function, but required by AWS Lambda).
+    """
     logger = logging.getLogger("MyLogger")
     logger.setLevel(logging.INFO)
-    logger.error("rcd10")
-    raise RuntimeError
+    # logger.error("rcd10")
+    try:
+        connection = connect()
+        updated_dfs_dict = read_updated_tables_from_s3(BUCKET_NAME)
+        # sql_query(connection, updated_dfs_dict)
+
+    except Exception as e:
+        logger.error(e)
+        raise RuntimeError
 
 
 def read_updated_tables_from_s3(bucket_name: str) -> dict[pd.DataFrame]:
@@ -54,29 +67,20 @@ def read_updated_tables_from_s3(bucket_name: str) -> dict[pd.DataFrame]:
     updated_tables_dict = read_state_file_from_s3(bucket_name)
 
     for table_name in updated_tables_dict:
-        if updated_tables_dict[table_name]: # if table has been updated
+        # if table has been updated
+        if updated_tables_dict[table_name]:
+            # list all the files in specified table's AWS folder
             list_of_filenames = list_files_from_s3_folder(bucket_name, table_name)
+            # extract lastest counter from the list of filenames
             counter, _ = return_latest_counter_and_timestamp_from_filenames(table_name, list_of_filenames)
+            # convert latest parquet file to dataframe
             df = get_parquet_dataframe_from_s3(bucket_name, table_name, counter)
+            # put each dataframe as key value pair in updated_dataframes
             updated_dataframes[table_name] = df
 
-    print(updated_dataframes)
+    # print(updated_dataframes)
     return updated_dataframes
 
-# def read_parquet_from_processed_bucket(bucket_name, folder_name, filename):
-#     s3Client = boto3.client('s3')
-#     # objects = s3Client.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
-#     obj = s3Client.get_object(Bucket=bucket_name, Key=filename) # objects['Contents'][-1]['Key']
-#     parquet_file = pq.ParquetFile(BytesIO(obj['Body'].read()))
-#     df = parquet_file.read().to_pandas()
-#     print(df)
-#     return df
 
-
-
-
-
-
-
-
-read_updated_tables_from_s3('data-detox-processed-bucket')
+if __name__ == "__main__":
+    lambda_handler3("test", "context")
